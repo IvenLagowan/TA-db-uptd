4a885f8eba88d07948a08faa742b75e8cd2b9913
iven lagowan <johndoe@example.com>
2023-09-28 05:42:55 +0900

    update file day 00

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+
+// Koneksi php
+
+$koneksi = mysqli_connect("localhost", "root",  "", "db_sekolah");
+
+
+// Cek Koneksi
+// if (mysqli_connect_errno()){
+// echo "Gagal koneksi ke database";
+// } else {
+//   echo "Berhasi koneksi";
+// }
+
+// url induk
+$main_url = "https://localhost/sekolah";
+
+
+
+
+
+
+
+
+
+
+?>
